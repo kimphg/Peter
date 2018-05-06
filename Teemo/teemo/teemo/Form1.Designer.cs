9f8328f399f5c6b6fc7da6015de8dd5f32ff5225
Phuong Phung <pkp013@gmail.com>
2018-05-06 12:50:01 +0700

    pkp

--- Teemo/teemo/teemo/Form1.Designer.cs
+++ Teemo/teemo/teemo/Form1.Designer.cs
@@ -33,8 +33,8 @@
             this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -63,6 +63,16 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(361, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(221, 58);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Enable selected items";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // button2
             // 
             this.button2.BackgroundImage = global::teemo.Properties.Resources.temooIcon;
@@ -76,16 +86,6 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(361, 76);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(221, 58);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Enable selected items";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
             // button4
             // 
             this.button4.BackgroundImage = global::teemo.Properties.Resources.temooIcon;
@@ -93,7 +93,7 @@
             this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button4.Location = new System.Drawing.Point(0, 0);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(598, 355);
+            this.button4.Size = new System.Drawing.Size(604, 304);
             this.button4.TabIndex = 4;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
@@ -102,7 +102,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(598, 355);
+            this.ClientSize = new System.Drawing.Size(604, 304);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
